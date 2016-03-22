6168b2be153e1bbb6ba70d372d4d33cdf56953ae
flrivas <flrivas.sm@gmail.com>
2016-03-22 17:24:46 -0300

    Added files via upload

---
+++ bd22.03.sql
@@ -0,0 +1,11 @@
+﻿create table login(
+id serial,
+login varchar(10),
+senha varchar(10),
+primary key (id)
+
+)
+
+insert into login (login, senha) values ('fernando', '123')
+
+select * from login
